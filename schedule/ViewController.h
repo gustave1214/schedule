f95767603425eef85a1faae386f9966e040c092b
guusteuwen929 <guusteuwen929@gmail.com>
2016-05-30 05:36:18 +0800

    Initial Commit

---
+++ schedule/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  schedule
+//
+//  Created by Prince on 5/30/16.
+//  Copyright © 2016 Pavel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ schedule/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  schedule
+//
+//  Created by Prince on 5/30/16.
+//  Copyright © 2016 Pavel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
